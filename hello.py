558c9a8cbd73b62888a518b77497109d7f435464
Cycl0 <lucas.cyra@etec.sp.gov.br>
2024-10-08 00:37:24 +0000

    Semana 02. Estrutura básica da aplicação

--- hello.py
+++ hello.py
@@ -1,25 +1,41 @@
 from flask import Flask, request, make_response, redirect, abort
 app = Flask(__name__)
 
+aula = "Aula 030"
+
 @app.route('/')
 def index():
     return '''
-            <h1>Hello World!</h1>
-            <p>Lucas Kenzo Cyra</p>
-            <p>PT3025764</p>
-           '''
+            <h1>Avaliação contínua: {}</h1>
+            <ul>
+                <li><a href="https://cyclon.pythonanywhere.com/"> Home </a></li>
+                <li><a href="https://cyclon.pythonanywhere.com/user/Lucas%20Kenzo%20Cyra/PT3025764/IFSP"> Identificação </a></li>
+                <li><a href="https://cyclon.pythonanywhere.com/contextorequisicao"> Contexto da requisição </a></li>
+            </ul>
+           '''.format(aula)
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.route('/user/<name>/<id>/<instution>')
+def user(name, id, instution):
+    return '''
+            <h1>Avaliação contínua: {}</h1>
+            <h2>Aluno: {}</h2>
+            <h2>Prontuário: {}</h2>
+            <h2>Instituição: {}</h2>
+            <a href="https://cyclon.pythonanywhere.com/"> Voltar </a>
+           '''.format(aula, name, id, instution)
 
 @app.route('/contextorequisicao')
 def contextorequisicao():
     user_agent = request.headers.get('User-Agent')
+    ip_remote = request.remote_addr
+    host = request.host_url
     return '''
-              <h1>Your browser is</h1>
-              <p>{}</p>
-           '''.format(user_agent)
+              <h1>Avaliação contínua: {}</h1>
+              <h2>Seu navegador é: {}</h2>
+              <h2>O IP do cumputador remoto é: {}</h2>
+              <h2>O host da aplicação é: {}</h2>
+              <a href="https://cyclon.pythonanywhere.com/"> Voltar </a>
+           '''.format(aula, user_agent, ip_remote, host)
 
 @app.route('/codigostatusdiferente')
 def codigostatusdiferente():
@@ -29,9 +45,11 @@ def codigostatusdiferente():
 def objetoresposta():
     answer = '42'
     resp = make_response('''
+                            <h1>Avaliação contínua: {}</h1>
                             <h1>This document carries a cookie!</h1>
-                            <p>Cookie value to add: {}</p>
-                         '''.format(answer))
+                            <h2>Cookie value to add: {}</h2>
+                            <a href="https://cyclon.pythonanywhere.com/"> Voltar </a>
+                         '''.format(aula, answer))
     resp.set_cookie('the_answer_to_everything', answer)
     return resp
 
