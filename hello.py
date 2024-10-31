d4037434c372ef8a22593ff8c08c901ad12f3bb7
Cycl0 <lucas.cyra@etec.sp.gov.br>
2024-10-31 02:59:17 +0000

    Semana 05. Formulários web

--- hello.py
+++ hello.py
@@ -1,21 +1,36 @@
-from flask import Flask, request, make_response, redirect, abort, render_template
+from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'AAAA@@@@3333$$$$'
 
-aula = "Aula 030"
+aula = "Aula 050"
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you havee changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), aula=aula)
 
 @app.route('/user/<name>/<id>/<inst>/')
 def user(name, id, inst):
-    return render_template('user.html', name=name, id=id, inst=inst)
+    return render_template('user.html', name=name, id=id, inst=inst, aula=aula)
 
 @app.route('/rotainexistente')
 def rotainexistente():
@@ -26,7 +41,7 @@ def contextorequisicao(name):
     user_agent = request.headers.get('User-Agent')
     ip = request.remote_addr
     host = request.host_url
-    return render_template('contextorequisicao.html', name=name, user_agent=user_agent, ip=ip, host=host)
+    return render_template('contextorequisicao.html', name=name, user_agent=user_agent, ip=ip, host=host, aula=aula)
 
 @app.route('/codigostatusdiferente')
 def codigostatusdiferente():
